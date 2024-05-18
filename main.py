067bac4350a16c8a77821619bccfcd4431ead9df
Andrimarciely <andrimarciely@gmail.com>
2024-05-18 11:53:39 -0400

    intial commit - basic struct

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+from dataclasses import dataclass
+from resource import data
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+
+# TODO 1.  Prompt user by asking “What would you like? (espresso/latte/cappuccino)
+# TODO 2. Turn off the Coffee Machine by entering “off” to the prompt.
+# TODO 3. Print report of all coffee machine resources
+# TODO 4. Check resources sufficient?
+# TODO 5. Process coins.
+# TODO 6. Check transaction successful?
+# TODO 7. Make Coffee.
+@dataclass
+class CoffeeMachine:
+    def __init__(self, resources, coins):
+        self.resources = []
+        self.coins = []
+
+
+class Resource:
+    description: str
+    volume: float
+    unit: str
+
+
+class Coins:
+    type: str
+    quantity: int
+    value: float
