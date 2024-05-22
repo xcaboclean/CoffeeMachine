9e2ea4795ea10a18d08c37a33ec44ebbc790aae4
Andrimarciely <andrimarciely@gmail.com>
2024-05-21 22:05:15 -0400

    intial commit - basic struct

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from dataclasses import dataclass
-from resource import data
+from data import resources
 
 
-# TODO 1.  Prompt user by asking “What would you like? (espresso/latte/cappuccino)
+# TODO 1.  rompt user by asking “What would you like? (espresso/latte/cappuccino)
 # TODO 2. Turn off the Coffee Machine by entering “off” to the prompt.
 # TODO 3. Print report of all coffee machine resources
 # TODO 4. Check resources sufficient?
 # TODO 5. Process coins.
 # TODO 6. Check transaction successful?
 # TODO 7. Make Coffee.
-@dataclass
+
 class CoffeeMachine:
     def __init__(self, resources, coins):
         self.resources = []
